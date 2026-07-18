f43f23534bba1b67132b48c85c3be38f18f450c2
imdipo <imammuhammaddiponegoro@gmail.com>
2026-07-18 08:12:10 +0700

    added order by, so the the data would be sorted

--- repositories/all_log.py
+++ repositories/all_log.py
@@ -2,5 +2,6 @@ from sqlalchemy.orm import Session
 from models.log_duit import LogDuit
 
 def get_all_logs(db: Session):
-    return db.query(LogDuit).all()
+    return db.query(LogDuit).order_by(LogDuit.waktu_transaksi.desc()).all()
+
 
