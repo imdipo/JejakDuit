a08fca7d402a1ccb69cf9a5005ca3a917408e26c
imdipo <imammuhammaddiponegoro@gmail.com>
2026-07-20 11:49:36 +0700

    make it more readable for hermes

--- repositories/weekly_report.py
+++ repositories/weekly_report.py
@@ -4,4 +4,20 @@ from models.log_duit import LogDuit
 
 def laporan_mingguan(db:Session):
     minggu_lalu = datetime.now() - timedelta(days=7)
-    return db.query(LogDuit).filter(LogDuit.waktu_transaksi >= minggu_lalu).all()
+    
+    list_transaksi = (
+        db.query(LogDuit).filter(LogDuit.waktu_transaksi >= minggu_lalu).order_by(LogDuit.waktu_transaksi.desc())
+        .all()
+    )
+
+    log_transaksi_rapih = []
+    for data in list_transaksi:
+        log_transaksi_rapih.append({
+            "tanggal transaksi": data.waktu_transaksi.strftime("%Y-%m-%d %H:%M"),
+            "kategori": data.jenis,
+            "penerima": data.penerima,
+            "nominal": f"Rp {data.nominal}",
+            "deskripsi": data.deskripsi
+        })
+    
+    return log_transaksi_rapih
