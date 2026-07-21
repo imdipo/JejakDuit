b6ebae3949446bd8c8f4e31c7f6a2d456c172c2e
imdipo <imammuhammaddiponegoro@gmail.com>
2026-07-21 22:34:00 +0700

    test insert dummy

--- test_insertdummy.py
+++ test_insertdummy.py
@@ -17,25 +17,3 @@ print("Semua data berhasil disimpan ke Postgres tanpa query SQL!", flush=True)
 
 db.close() 
 
-# def insert_data_bersih(list_data_email):
-#     # 1. Buka koneksi/session ke database
-  
-    
-#     try:
-#         # 2. Looping data email kamu yang berbentuk list of dict
-#         for data in list_data_email:
-            
-#             # 3. Ubah DICTIONARY menjadi OBJECT CLASS (Kuncinya di sini!)
-
-            
-#             # 4. Daftarkan object ini ke antrean database
-            
-        
-#         # 5. Ketuk palu! Kirim dan simpan semua data sekaligus ke Postgres
-
-        
-#     except Exception as e:
-#         db.rollback() # Batalkan semua jika ada 1 saja yang error (misal no. referensi kembar)
-#         print(f"Gagal insert data karena: {e}")
-#     finally:
-#         db.close() # Putuskan koneksi setelah selesai
